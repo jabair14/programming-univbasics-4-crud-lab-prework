267cee3d38d9b4155c8069c0735a0b1665e2ed2f
Jabair Khan <jabairk@gmail.com>
2021-01-24 01:11:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,7 +46,9 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    index_number = 5 
+    array [5]
 end
 
 def update_element_from_index(array, index_number, element)
