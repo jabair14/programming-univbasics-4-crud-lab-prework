b84009314b5c3f3916e29a8ce8efff42b1cc6833
Jabair Khan <jabairk@gmail.com>
2021-01-24 01:01:04 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,6 +33,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number = 4
   
 end
 
